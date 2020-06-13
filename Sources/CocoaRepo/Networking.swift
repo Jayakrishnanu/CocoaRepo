f43f70f20ebadd824699b513cf09695951422c0f
jayakrishnanu <jkrishnanu@gmail.com>
2020-06-13 08:42:46 -0700

    Update the networking call

--- Sources/CocoaRepo/Networking.swift
+++ Sources/CocoaRepo/Networking.swift
@@ -10,5 +10,23 @@ import Foundation
 extension CocoaCore {
     public class Networking {
         
+        /// Responsible for handalling all network calls
+        /// Warning: Must create before using any Public API's
+        public class Manager {
+            public init() { }
+            private let session = URLSession.shared
+            public func loadData(from url: URL, completionHandler: @escaping(NetworkResult<Data>) -> Void) {
+                let task = session.dataTask(with: url) { (data, response, error) in
+                    let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                    completionHandler(result)
+                }
+                task.resume()
+            }
+        }
+        
+        public enum NetworkResult<Value> {
+            case success(Value)
+            case failure(Error?)
+        }
     }
 }
--- Tests/CocoaRepoTests/CocoaNetworking.swift
+++ Tests/CocoaRepoTests/CocoaNetworking.swift
@@ -6,16 +6,37 @@
 //
 
 import XCTest
+@testable import CocoaRepo
 
 final class CocoaNetworking: XCTestCase {
 
+    func testLoadDataCall() {
+        let manager = CocoaCore.Networking.Manager()
+        guard let url = URL(string: "https://google.com") else {
+            return XCTFail("Could not create URL properly.")
+        }
+        let expectation = XCTestExpectation(description: "Called for data")
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnData):
+                XCTAssertNotNil(returnData, "Response data is not nil")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "Error loading results")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     static var allTests = [
-        ("testExample", testExample)
+        ("testExample", testExample),
+        ("testLoadDataCall", testLoadDataCall)
         ] 
  
 }
